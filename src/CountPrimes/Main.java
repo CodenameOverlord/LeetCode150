09b15738ccf4490d7685789c97b9414354f4a44b
CodenameOverlord <utsavkumardatta@gmail.com>
2024-04-27 00:45:16 +0530

    count primes basic and optimized soln

---
+++ src/CountPrimes/Main.java
@@ -0,0 +1,27 @@
+package CountPrimes;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(countPrimes(240));
+
+
+    }
+    public static int countPrimes(int n) {
+        if(n<3){
+            return 0;
+        }
+        int [] arr = new int [n];
+        for(int i = 2; i<n; ++i){
+            for(int j = i+i; j<n; j+=i){
+                arr[j]=-1;
+            }
+        }
+        int count = 0;
+        for(int i = 2; i<arr.length; ++i){
+            if(arr[i]!=-1)
+                count++;
+        }
+
+        return count;
+    }
+}
