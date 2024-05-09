20c5459d17dc6231b67cc23422622102c0b2310a
PF48H1T0\User <utsav.datta@wissen.com>
2024-05-09 10:22:13 +0530

    Inorder -tree recursive

--- src/InorderTree/Main.java
+++ src/InorderTree/Main.java
@@ -1,4 +1,42 @@
 package InorderTree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    public static void main(String[] args) {
+
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> solutionList = new ArrayList<>();
+        inorder(solutionList, root);
+        return solutionList;
+    }
+
+    public void inorder(List<Integer> result, TreeNode root){
+        if(root == null){
+            // result.add(null);
+            return;
+        }
+        else{
+            inorder(result, root.left);
+            result.add(root.val);
+            inorder(result, root.right);
+        }
+    }
 }
