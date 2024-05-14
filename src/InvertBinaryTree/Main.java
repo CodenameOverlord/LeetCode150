cad6e90bcc21cc10d04bcab56029c750a19a2eb2
CodenameOverlord <utsavkumardatta@gmail.com>
2024-05-14 13:54:24 +0530

    invert binary tree code

---
+++ src/InvertBinaryTree/Main.java
@@ -0,0 +1,29 @@
+package InvertBinaryTree;
+
+public class Main {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null){
+            return root;
+        }
+        else{
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            invertTree(root.left);
+            invertTree(root.right);
+        }
+        return root;
+    }
+}
