8a26e59edcccbb5228c2d43d7eb7bcced8d26258
CodenameOverlord <utsavkumardatta@gmail.com>
2024-05-15 22:39:16 +0530

    Contains nearby duplicate

--- src/ContainsNearbyDuplicate/Main.java
+++ src/ContainsNearbyDuplicate/Main.java
@@ -1,20 +1,30 @@
 package ContainsNearbyDuplicate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(containsNearbyDuplicate(new int [] {1,2,34,5,1,5},2));
     }
+
+
     public static boolean containsNearbyDuplicate(int[] nums, int k) {
-        int count=0;
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j <= i + k && j < nums.length; j++){
-                if(nums[j] == nums[i]){
+        Map<Integer, Integer> hMap = new HashMap<>();
+        int j = 0;
+        for(int i =0; i< nums.length; ++i){
+            int currElement = nums[i];
+            if(hMap.containsKey(currElement)){
+                if(i-hMap.get(currElement)<=k){
                     return true;
                 }
-                count++;
-                if(count > 99999){
-                    return false;
-                }
+                hMap.put(currElement, i);
+            }
+            else{
+                hMap.put(currElement, i);
+            }
+            if(hMap.size()>=k+1){
+                hMap.remove(nums[j++]);
             }
         }
 
---
+++ src/ContainsNearbyDuplicate/OptimizedSoln.java
@@ -0,0 +1,20 @@
+package ContainsNearbyDuplicate;
+
+public class OptimizedSoln {
+//    public static boolean containsNearbyDuplicatePending(int[] nums, int k) {
+//        int count=0;
+//        for(int i = 0; i < nums.length; i++){
+//            for(int j = i + 1; j <= i + k && j < nums.length; j++){
+//                if(nums[j] == nums[i]){
+//                    return true;
+//                }
+//                count++;
+//                if(count > 99999){
+//                    return false;
+//                }
+//            }
+//        }
+//
+//        return false;
+//    }
+}
