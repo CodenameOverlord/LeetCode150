672a071e162966e4fcd3d3b33c69f66203b9ce33
CodenameOverlord <utsavkumardatta@gmail.com>
2024-05-19 19:09:46 +0530

    updated code of trailing zeroes

--- src/FactorialTrailingZeroes/OptimizedSolution.java
+++ src/FactorialTrailingZeroes/OptimizedSolution.java
@@ -7,7 +7,7 @@ public class OptimizedSolution {
 
     public static int trailingZeroes(int n) {
         int ans = 0;
-        for(int i =5; i<n; i=i*5 ){
+        for(int i =5; i<=n; i=i*5 ){
             ans+=n/i;
         }
 
