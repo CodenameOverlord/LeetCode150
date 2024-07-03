8e9d9bd4a4820ff39ac5d7152c55669972557669
CodenameOverlord <utsavkumardatta@gmail.com>
2024-07-03 23:29:53 +0530

    Kth smallest element best soln

--- src/KthSmallestElementInABst/Main.java
+++ src/KthSmallestElementInABst/Main.java
@@ -1,15 +1,17 @@
-public class kthSmallestElementInABst {
+package KthSmallestElementInABst;
+
+public class Main {
     public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 
     int count =0;
